1b17b52c6499f6700283f127adca48555bc78778
SyauqiAlifI <vstudio776@gmail.com>
2021-12-03 20:50:25 +0700

    hai

---
+++ app/src/main/java/com/aplikasi/portofolioappidn/androidporto/Portofolio.kt
@@ -0,0 +1,13 @@
+package com.aplikasi.portofolioappidn.androidporto
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Portofolio(
+    val id: Int = 0,
+    var title: String = "",
+    var poster: Int = 0,
+    var link: String = ""
+) : Parcelable
